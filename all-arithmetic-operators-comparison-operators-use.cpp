28779df7ece71de5ed6c6533709a183274985b51
Rahul kumawat <rahulkumawat9652@gmail.com>
2025-11-30 11:52:27 +0530

    all arithmetic operators comparison operators use

---
+++ all-arithmetic-operators-comparison-operators-use.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a=4,b=5;
+//arithmetic operators
+cout<<"value of a+b is "<<a+b;
+cout<<"\nvalue of a-b is "<<a-b;
+cout<<"\nvalue of a*b is "<<a*b;
+cout<<"\nvalue of a/b is "<<a/b;
+cout<<"\nvalue of a%b is "<<a%b;
+cout<<"\nvalue of --a is "<<--a;
+cout<<"\nvalue of ++a is "<<++a;
+cout<<"\nvalue of a++ is "<<a++;
+cout<<"\nvalue of a-- is "<<a--;
+
+// Comparison operators
+cout<<"value of a==b is "<<(a==b);
+cout<<"\nvalue of a!=b is "<<(a!=b);
+cout<<"\nvalue of a>=b is "<<(a>=b);
+cout<<"\nvalue of a<=b is "<<(a<=b);
+cout<<"\nvalue of a>b is "<<(a>b);
+cout<<"\nvalue of a<b is "<<(a<b);
+return 0;
+} 
