32494aeb8964242a09e1fcc936437f806632c667
Rahul kumawat <rahulkumawat9652@gmail.com>
2025-11-30 15:04:23 +0530

    two number sum subtract mutiply division program

---
+++ two-number-sum-subtract-mutiply-division-program.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a=4,b=5;
+cout<<"value of a+b is "<<a+b;
+cout<<"\nvalue of a-b is "<<a-b;
+cout<<"\nvalue of a*b is "<<a*b;
+cout<<"\nvalue of a/b is "<<a/b;
+cout<<"\nvalue of a%b is "<<a%b;
+return 0;
+}
