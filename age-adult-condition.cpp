fbe1a3c326d6132dd5c25277b404bf1c8cf2ccfc
Rahul kumawat <rahulkumawat9652@gmail.com>
2025-12-01 18:45:41 +0530

    age aduld condition

---
+++ age-adult-condition.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+int age;
+cout<<"tell me your age\n";
+cin>>age;
+if(age<18){
+cout<<"not adult";
+}
+else if(age==18){
+cout<<"adult";
+}
+else{
+cout<<"adult";
+}
+return 0;
+}
