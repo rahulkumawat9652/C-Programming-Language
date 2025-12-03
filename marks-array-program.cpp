b02f4227f8fbdc625f4933458a532bce3dd93fec
Rahul kumawat <rahulkumawat9652@gmail.com>
2025-12-03 18:21:09 +0530

    marks array program

---
+++ marks-array-program.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+int marks[] = {23,45,567,35};
+cout<<marks[0]<<endl;
+cout<<marks[1]<<endl;
+cout<<marks[2]<<endl;
+cout<<marks[3]<<endl;
+return 0;
+}
