4aa9787480c755ad2d952f1cbb5d2003b772848c
Rahul kumawat <rahulkumawat9652@gmail.com>
2025-12-04 14:16:58 +0530

    two number sum using function

---
+++ two-number-sum-using-function-.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int sum(int a,int b){
+int c = a+b;
+return c;
+}
+int main(){
+int num1,num2;
+cout<<"enter first number"<<endl;
+cin>>num1;
+cout<<"enter second number"<<endl;
+cin>>num2;
+cout<<"The sum is "<<sum(num1,num2);
+return 0;
+}
