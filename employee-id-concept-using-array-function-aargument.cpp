8560d3fa92116eb746984b462aec98a0379ff1bb
Rahul kumawat <rahulkumawat9652@gmail.com>
2025-12-15 10:11:48 +0530

    employee id concept using array function aargument

---
+++ employee-id-concept-using-array-function-aargument.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+class Employee{
+int id;
+int salary;
+public:
+void setId(void){
+salary =122;
+cout<<"Enter the Id of employee"<<endl;
+cin>>id;
+}
+void getId(void){
+cout<<"The id of this employee is "<<id<<endl;
+}
+};
+int main(){
+Employee fb[10];
+for(int i=0;i<10;i++){
+fb[i].setId();
+fb[i].getId();
+}
+return 0;
+}
