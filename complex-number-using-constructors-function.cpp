fed1b0cbbe5a7b944ba549078e6080e40e2d7b54
Rahul kumawat <rahulkumawat9652@gmail.com>
2025-12-17 19:29:14 +0530

    complex number using constructors function

---
+++ complex-number-using-constructors-function.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+class Complex {
+int a,b;
+public:
+Complex(void);
+void printNumber(){
+cout<<"your number is "<<a<<" + "<<b<<"i"<<endl;
+}
+};
+Complex :: Complex(void){
+a=10;
+b=0;
+}
+int main(){
+Complex c;
+c.printNumber();
+return 0;
+}
