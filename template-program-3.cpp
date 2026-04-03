12b17e712ffb44c0799850d822061f5e88c3cc7f
Rahul kumawat <rahulkumawat9652@gmail.com>
2026-04-03 15:28:32 +0530

    template program 3

---
+++ template-program-3.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+void display(vector<int> &v){
+for(int i = 0;i<v.size();i++){
+cout<<v[i]<<" ";
+}
+cout<<endl;
+}
+int main(){
+vector<int> vec1;
+int element;
+for(int i= 0; i<4; i++){
+cout<<"Enter an element to add to the vector: ";
+cin>>element;
+vec1.push_back(element);
+}
+display(vec1);
+vector<int>::iterator iter = vec1.begin();
+vec1.insert(iter , 566);
+display(vec1);
+return 0;
+}
