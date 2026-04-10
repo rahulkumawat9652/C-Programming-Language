7f3c15b7884a1b5be9bd4a3d91d45d648e28d776
Rahul kumawat <rahulkumawat9652@gmail.com>
2026-04-10 20:42:17 +0530

    function objects increase order program

---
+++ function-objects-increase-order-program.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<functional>
+#include<algorithm>
+using namespace std;
+int main(){
+int arr[] = {1,76,3,8,45,2};
+sort(arr,arr+6);
+for(int i = 0; i<6 ;i++){
+cout<<arr[i]<<endl;
+}
+return 0;
+}
